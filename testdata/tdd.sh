64169d5235ec7209d3a4d2ccbc566dc1629e458e
Phil Hanna <ph1204@gmail.com>
2024-01-14 09:46:22 -0500

    Get user agent from ua.sh

--- testdata/tdd.sh
+++ testdata/tdd.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 ./ua.sh
-curl \
--A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
-(KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36" \
-"https://www.google.com/search?q=test+driven+development" \
--o tdd.html
+curl -L \
+    -A "$UA" \
+    "https://google.com/search?q=test+driven+development" \
+    -o tdd.html
