d09588ba4669e590383c5e3c7ff2098f85bd406f
Phil Hanna <ph1204@gmail.com>
2024-01-14 17:52:06 -0500

    Working on parser

--- parser.go
+++ parser.go
@@ -1,8 +1,9 @@
 package search
 
 import (
-	// "golang.org/x/net/html"
-	// "strings"
+	"strings"
+
+	"golang.org/x/net/html"
 )
 
 // ---------------------------------------------------------------------
@@ -15,7 +16,7 @@ type Link struct {
 }
 
 type HTMLDoc struct {
-	Data  string // The HTML of the document
+	HTML  string // The HTML of the document
 	Links []Link // The links that the parser finds
 }
 
@@ -27,15 +28,50 @@ type HTMLDoc struct {
 // parses it for links, and returns a pointer to it
 func NewHTMLDoc(input string) (*HTMLDoc, error) {
 	p := new(HTMLDoc)
-	p.Data = input
+	p.HTML = input
 	p.Links = make([]Link, 0)
 	err := p.parse()
-	if err != nil {
-		return nil, err
-	}
-	return p, nil
+	return p, err
 }
 
 func (doc *HTMLDoc) parse() error {
+	elemRoot, err := html.Parse(strings.NewReader(doc.HTML))
+	if err != nil {
+		return err
+	}
+	err = handleElementNode(elemRoot)
+	if err != nil {
+		return err
+	}
 	return nil
 }
+
+func handleElementNode(elem *html.Node) error {
+	if elem.Data == "div" {
+		class := getAttribute(elem, "class")
+		if isLinkDiv(class) {
+			_ = class
+		}
+	}
+	for child := elem.FirstChild; child != nil; child = child.NextSibling {
+		switch child.Type {
+		case html.ElementNode:
+			handleElementNode(child)
+		}
+	}
+	return nil
+}
+
+func getAttribute(node *html.Node, key string) string {
+	for _, attr := range node.Attr {
+		if attr.Key == key {
+			return attr.Val
+		}
+	}
+	return ""
+}
+
+func isLinkDiv(class string) bool {
+	return strings.Contains(class, "egMi0") && strings.Contains(class, "kCrYT")
+}
+
