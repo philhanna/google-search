f2f7887840da8111319e801162c9995a3863d864
Phil Hanna <ph1204@gmail.com>
2024-01-14 11:20:55 -0500

    Recreate HTML

--- cmd/htmlparser/main.go
+++ cmd/htmlparser/main.go
@@ -8,25 +8,49 @@ import (
 	"golang.org/x/net/html"
 )
 
+var level = 0
+
 func main() {
 	s := `<p>Links:</p><ul><li><a href="foo">Foo</a><li><a href="/bar/baz">BarBaz</a></ul>`
 	doc, err := html.Parse(strings.NewReader(s))
 	if err != nil {
 		log.Fatal(err)
 	}
-	var f func(*html.Node)
-	f = func(n *html.Node) {
-		if n.Type == html.ElementNode && n.Data == "a" {
-			for _, a := range n.Attr {
-				if a.Key == "href" {
-					fmt.Println(a.Val)
-					break
-				}
-			}
-		}
-		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			f(c)
-		}
+	printDocumentNode(doc)
+}
+
+func indent() string {
+	return strings.Repeat("  ", level)
+}
+
+func printNode(node *html.Node) {
+	// fmt.Printf("node type: %d, data: %q\n", node.Type, node.Data)
+	switch node.Type {
+	case html.TextNode:
+		printTextNode(node)
+	case html.DocumentNode:
+		printDocumentNode(node)
+	case html.ElementNode:
+		printElementNode(node)
 	}
-	f(doc)
+}
+
+func printTextNode(node *html.Node) {
+	fmt.Printf("%s%s\n", indent(), node.Data)
+}
+
+func printDocumentNode(node *html.Node) {
+	for child := node.FirstChild; child != nil; child = child.NextSibling {
+		printNode(child)
+	}
+}
+
+func printElementNode(node *html.Node) {
+	fmt.Printf("%s<%s>\n", indent(), node.Data)
+	level++
+	for child := node.FirstChild; child != nil; child = child.NextSibling {
+		printNode(child)
+	}
+	level--
+	fmt.Printf("%s</%s>\n", indent(), node.Data)
 }
