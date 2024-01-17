c57f65e2c0fd64c0f0961f6ff85e7cb646fde1c8
Phil Hanna <ph1204@gmail.com>
2024-01-16 20:37:53 -0500

    Added mainline

--- cmd/links/google-search.go
+++ cmd/links/google-search.go
@@ -1,5 +1,44 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
 
+	search "github.com/philhanna/google_search"
+)
+const USAGE = `usage: google-search [QUERY]
+
+Performs a Google search with the specified query. Returns a JSON array of the links found.
+
+positional arguments:
+  query          The query to be performed
+
+`
+func main() {
+	flag.Usage = func() {
+		fmt.Fprint(os.Stderr, USAGE)
+	}
+	flag.Parse()
+	if flag.NArg() != 1 {
+		log.Fatal("No query specified")
+	}
+	query := flag.Arg(0)
+	doc, err := search.Download(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println ("[")
+	for i, link := range doc.Links {
+		comma := ","
+		if i == len(doc.Links) - 1 {
+			comma = ""
+		}
+		fmt.Println("  {")
+		fmt.Printf("    %q:%q,\n", "title", link.Title)
+		fmt.Printf("    %q:%q\n", "url", link.URL)
+		fmt.Printf("  }%s\n", comma)
+	}
+	fmt.Println ("]")
 }
--- htmldoc.go
+++ htmldoc.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	GOOGLE_URL = "https://www.google.com/search?q="
-	UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+	UA         = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 )
 
 // ---------------------------------------------------------------------
